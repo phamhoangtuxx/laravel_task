8ac88b549d81509f502ffe29e713aea1513ef8ef
Pham Hoang Tu <phamhoangtuxx@gmail.com>
2022-12-15 20:22:19 +0700

    push code

--- app/Http/Controllers/CurlController.php
+++ app/Http/Controllers/CurlController.php
@@ -15,15 +15,7 @@ class CurlController extends Controller
     public function getSubjects()
     {
 
-        // $response = Http::get('https://apps.vietjack.com:8081/api/books');
-
-        // if ($response == true) {
-        //     $response->successful();
-        //     return response()->json(['data' => 'co du lieu']);
-        // }
-
-
-        $url = 'https://apps.vietjack.com:8081/api/books';
+        $url = 'https://apps.vietjack.com:8081/api/subjects';
         $curl = curl_init($url);
 
         $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwcHMudmlldGphY2suY29tOjgwODEvYXBpL2F1dGgvc29jaWFsLWxvZ2luIiwiaWF0IjoxNjcxMDI1NDgxLCJleHAiOjE2NzE2MzAyODEsIm5iZiI6MTY3MTAyNTQ4MSwianRpIjoieWthUzN2WDZ2czJPandLUCIsInN1YiI6MzM5ODczNCwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.PkRHVlu0LeOXEp5GxYOdNl3zrg65jD9-LBqSbBiOW1s';
